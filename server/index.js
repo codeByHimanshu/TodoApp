63e4c70130391e5e42da520b0675a3255772bb2c
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-01-14 18:27:10 +0530

    getting todoo

--- server/index.js
+++ server/index.js
@@ -1,40 +1,59 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const {creatTodo} = require("./models/todoSchema")
-const {todo} = require('./config/db')
-dotenv.config()
-app.use(express.json())
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const { createTodo, updateTodo } = require("./models/todoSchema");
+const { todo } = require("./config/db");
+dotenv.config();
+app.use(express.json());
 
-
-app.get('/gettodo',function (req,res) {
-    
+app.get("/get", function (req, res) {
+const todos = todo.findone({  
 })
 
-app.post('/craete', function(req,res){
-const craetePayload = req.body
-const parsepayload = creatTodo.safeParse(craetePayload)
-if (! parsepayload.success) {
-    res.status(404).json({
-        msg:"invali inputs are provided"
-    })
-    return
-}
-})
-
-app.put('/update',function(req,res) {
-    const craetePayload = req.body
-const parsepayload = creatTodo.safeParse(craetePayload)
-if (! parsepayload.success) {
-    res.status(404).json({
-        msg:"invali inputs are provided"
-    })
-    return
-}
-})
-const port = process.env.PORT
-app.listen(port, () => {
-    console.log(`server is running on the port ${port}`);
 });
 
+app.post("/create", async function (req, res) {
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+  if (!parsePayload.success) {
+    res.status(404).json({
+      msg: "invalid inputs are provided",
+    });
+    return;
+  }
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({
+    msg: "todo created",
+  });
+});
 
+app.put("/update", async function (req, res) {
+  const updatePayload = req.body;
+  const parsepayload = updateTodo.safeParse(updatePayload);
+  if (!parsepayload.success) {
+    res.status(404).json({
+      msg: "invali inputs are provided",
+    });
+    return;
+  }
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.status(200).json({
+    msg: "todo updated",
+  });
+});
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server is running on the port ${port}`);
+});
